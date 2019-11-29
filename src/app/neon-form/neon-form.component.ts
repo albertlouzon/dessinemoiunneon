4f7090ff7ae16c6ed3623d575ff17352406f8916
Albert Louzon <albertlouzon@gmail.com>
2019-11-29 19:01:22 +0100

    step 1 step 2

--- src/app/neon-form/neon-form.component.ts
+++ src/app/neon-form/neon-form.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-neon-form',
   templateUrl: './neon-form.component.html',
-  styleUrls: ['./neon-form.component.scss']
+  styleUrls: ['./neon-form.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NeonFormComponent implements OnInit {
   userChoices = {};
+  styleSelected = 1;
+  textInput = '';
+  imageFile = '';
+  imageAdditionalInfo = '';
   constructor() { }
 
   ngOnInit() {
+  }
+  onChangeTextTitle(value: string) {
+    if(value.trim() !== '') {
+      this.textInput = value;
+    }
+  }
+  onSelectFile(event) {
+    console.log('file selected' , event);
+    if(event.target.files) {
+      console.log('file selected' , event.target.files);
+      this.imageFile =  event.target.files[0];
+    }
+  }
+  onChangeAdditionnalInfo(value: string) {
+    if(value.trim() !== '') {
+      this.imageAdditionalInfo = value;
+    }
+  }
+  onSubmitForm(){
+    const payload: Array<{choice: string, data : {}}> = []
+    if(this.userChoices[0] === 'text') {
+      payload.push({choice: 'text', data: {value: this.textInput, style: this.styleSelected}})
+
+    } else {
+      payload.push({choice: 'image', data: {file: this.imageFile, info: this.imageAdditionalInfo}})
+
+    }
+    console.log('payload : ', payload)
   }
 
   onCompleteStep(step: number, choice: string, data: any) {

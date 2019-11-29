7378989375f8ba3ffd7f1f9ce203b068d6b398bd
Albert Louzon <albertlouzon@gmail.com>
2019-11-29 16:34:23 +0100

    added form header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NeonFormComponent } from './neon-form/neon-form.component';
 import { ArchwizardModule } from 'angular-archwizard';
+import { FormHeaderComponent } from './form-header/form-header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NeonFormComponent
+    NeonFormComponent,
+    FormHeaderComponent
   ],
   imports: [
     BrowserModule,

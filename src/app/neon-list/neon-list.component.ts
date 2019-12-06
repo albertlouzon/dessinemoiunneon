0be1a3c0e4e1fc272872131c0a26c7092847f3e4
Albert Louzon <albertlouzon@gmail.com>
2019-12-06 18:27:51 +0100

    hazak

--- src/app/neon-list/neon-list.component.ts
+++ src/app/neon-list/neon-list.component.ts
@@ -16,25 +16,27 @@ export class NeonListComponent implements OnInit {
   neonList = [];
   commandMode = false;
   user = {};
-  commandInfos: {nom: string, prénom: string, société: string, adresse: string, ville: string, ['code-postal']: string, pays: string, téléphone: string} 
-      = {nom: '', prénom: '', société: '', adresse: '', ville: '', ['code-postal']: '', pays: '', téléphone: ''};;
+  commandInfos: { nom: string, prénom: string, société: string, adresse: string, ville: string, ['code-postal']: string, pays: string, téléphone: string }
+    = { nom: '', prénom: '', société: '', adresse: '', ville: '', ['code-postal']: '', pays: '', téléphone: '' };;
   errorMessage = 'Des champs sont incomplets...'
   commandFailed = false;
   loading = false;
+  payInfos = {};
+  payMode = false;
   ngOnInit() {
 
     // this.axiosClient.get('http://localhost:5555').then(((res) => {alert(res)}))
     this.fetchCommands();
   }
 
-  fetchCommands(){
+  fetchCommands() {
     this.neonList = []
     this.loading = true;
     this.getConfig().subscribe((res: Array<Object>) => {
-      if(res) {
+      if (res) {
         this.loading = false;
         console.log('ok ', res, localStorage.getItem('email'))
-        if(!res.find(x => x['email'] === localStorage.getItem('email')) || !localStorage.getItem('email')) {
+        if (!res.find(x => x['email'] === localStorage.getItem('email')) || !localStorage.getItem('email')) {
           alert('you are not logged in. T as rien a faire la va t inscrire');
           currentView['caca'] = 'login'
         } else {
@@ -42,13 +44,13 @@ export class NeonListComponent implements OnInit {
           var commands = res.find(x => x['email'] === localStorage.getItem('email'))['commands']
           this.user = res.find(x => x['email'] === localStorage.getItem('email'));
           console.log(commands)
-  
+
           commands.forEach((command) => {
             this.neonList.push(command)
           })
         }
-   
-      } 
+
+      }
       console.log('User data after fetch : ', this.neonList);
     })
   }
@@ -63,14 +65,14 @@ export class NeonListComponent implements OnInit {
 
   dlFile(url) {
     // window.location.href=url;
-    this.http.get(url, 
-      {responseType: 'blob'}).subscribe(data => saveAs(data, `pdf report.pdf`));
+    this.http.get(url,
+      { responseType: 'blob' }).subscribe(data => saveAs(data, `pdf report.pdf`));
   }
 
   goToNeonDetail(neonPayload) {
     console.log('the neon to see: ', neonPayload);
-    if(neonPayload && neonPayload.state !== 'created') {
-      this.neonSelected = neonPayload 
+    if (neonPayload && neonPayload.state !== 'created') {
+      this.neonSelected = neonPayload
     } else {
       alert('le DT est en cours de préparation')
     }
@@ -79,35 +81,88 @@ export class NeonListComponent implements OnInit {
   onChangeCommandInfo(field, value: string) {
     this.commandInfos[field] = value;
   }
-  openCommand(){
-    this.commandFailed = false;
-    this.commandInfos = {nom: '', prénom: '', société: '', adresse: '', ville: '', ['code-postal']: '', pays: '', téléphone: ''};
-    this.commandMode = !this.commandMode;
+
+
+  previous() {
+    this.commandMode = false;
+  }
+  openCommand() {
+    if (this.neonSelected.state === "commandé") {
+      this.commandFailed = false;
+      this.payInfos = { nom: '', prénom: '', société: '', adresse: '', ville: '', ['code-postal']: '', pays: '', téléphone: '' };
+      this.commandMode = false;
+      // this.payMode = true;
+      this.loading = true;
+      // setTimeout(() => {
+      //   this.loading = false;
+
+      // }, 3000);
+      this.loadStripe();
+      var handler = (<any>window).StripeCheckout.configure({
+        key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+        locale: 'auto',
+        token:  (token: any) => {
+          // You can access the token ID with `token.id`.
+          // Get the token ID to your server-side code for use.
+          console.log(token)
+          alert('Token Created!!');
+          this.payMode = true;
+          this.removeStripe();
+        }
+      });
+   
+      handler.open({
+        name: 'Stripe payment for:  ' + this.neonSelected.text,
+        // description: '2 widgets',
+        amount: this.neonSelected.price * 100
+      });
+    } else {
+      this.commandFailed = false;
+      this.commandInfos = { nom: '', prénom: '', société: '', adresse: '', ville: '', ['code-postal']: '', pays: '', téléphone: '' };
+      this.commandMode = !this.commandMode;
+
+    }
+
   }
 
   command() {
-      for(const field in this.commandInfos) {
-        if(this.commandInfos[field].trim() === '') {
-          this.commandFailed = true;
-          this.errorMessage = 'Oops ! Le champ ' + field.toUpperCase() + ' est manquant...';
-          return;
-        }
+    for (const field in this.commandInfos) {
+      if (this.commandInfos[field].trim() === '') {
+        this.commandFailed = true;
+        this.errorMessage = 'Oops ! Le champ ' + field.toUpperCase() + ' est manquant...';
+        return;
       }
-      console.log('the command is valid :', this.commandInfos);
-      this.commandFailed = false;
-      this.loading = true;
-      this.user['commands'].find(c => c.id === this.neonSelected.id)['commandInfo'] = this.commandInfos;
-      this.user['commands'].find(c => c.id === this.neonSelected.id)['state'] = 'commandé';
-      this.http.put('https://neon-server.herokuapp.com/users/' + this.user['id'], this.user).subscribe((res) => {
-        alert('DT uploaded. Email sent')
+    }
+    console.log('the command is valid :', this.commandInfos);
+    this.commandFailed = false;
+    this.loading = true;
+    this.user['commands'].find(c => c.id === this.neonSelected.id)['commandInfo'] = this.commandInfos;
+    this.user['commands'].find(c => c.id === this.neonSelected.id)['state'] = 'commandé';
+    this.http.put('https://neon-server.herokuapp.com/users/' + this.user['id'], this.user).subscribe((res) => {
+      alert('Néon commandé. Email sent')
+      this.fetchCommands();
+
+    }, err => {
+      if (err.status === 200) {
+        alert('Néon commandé. Email sent')
         this.fetchCommands();
+      }
+    });
 
-      }, err => {
-        if(err.status === 200) {
-          alert('DT uploaded. Email sent')
-          this.fetchCommands();
-        }
-      });
+  }
+  removeStripe() {
+    // !window.document.getElementById('stripe-script')
+    document.getElementById("stripe-script").outerHTML = "";
 
-}
+  }
+  loadStripe() {
+
+    if (!window.document.getElementById('stripe-script')) {
+      var s = window.document.createElement("script");
+      s.id = "stripe-script";
+      s.type = "text/javascript";
+      s.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(s);
+    }
+  }
 } 

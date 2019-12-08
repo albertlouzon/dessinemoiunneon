540f747aeb4d363e523fb64774c9d1f390e71ad5
Albert Louzon <albert.louzon@darillium.io>
2019-12-08 19:50:15 +0200

    ec responsive

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -21,11 +21,10 @@ export class AdminComponent implements OnInit, OnDestroy {
   numOfPayes = 0;
 
   ngOnInit() {
-       // this.axiosClient.get('http://localhost:5555').then(((res) => {alert(res)}))
 this.fetchCommands();
 this.pollInterval = setInterval(() => {
   this.fetchCommands();
-}, 80000)
+}, 80000);
   }
 
   ngOnDestroy() {
@@ -34,42 +33,36 @@ this.pollInterval = setInterval(() => {
   goToDetail(command) {
     this.neonSelected = command;
     this.commandPrice = 0;
-    console.log('command selected by admin : ', command)
+    console.log('command selected by admin : ', command);
   }
 
   filterCommand(type) {
-    console.log(type)
-    if(type === 'Created') {
-      this.neonList = this.cachedList.filter(c => c.state === 'created')
-    }
-    else  if(type === 'DT disponible') {
-      this.neonList = this.cachedList.filter(c => c.state === 'DT disponible')
-    }
-    else  if(type === 'Payé') {
-      this.neonList = this.cachedList.filter(c => c.state === 'payé')
-    }
-    else  if(type === 'Consumer') {
-      this.neonList = this.cachedList.filter(c => c.type === 'consumer')
-    }
-    else  if(type === 'Business') {
-      this.neonList = this.cachedList.filter(c => c.type === 'business')
-    }
-    
-    else if(type === 'all') {
+    console.log(type);
+    if (type === 'Created') {
+      this.neonList = this.cachedList.filter(c => c.state === 'created');
+    } else  if (type === 'DT disponible') {
+      this.neonList = this.cachedList.filter(c => c.state === 'DT disponible');
+    } else  if (type === 'Payé') {
+      this.neonList = this.cachedList.filter(c => c.state === 'payé');
+    } else  if (type === 'Consumer') {
+      this.neonList = this.cachedList.filter(c => c.type === 'consumer');
+    } else  if (type === 'Business') {
+      this.neonList = this.cachedList.filter(c => c.type === 'business');
+    } else if (type === 'all') {
       this.neonList = this.cachedList;
     } else {
-      this.neonList = this.cachedList.filter(c => c.typo === type)
+      this.neonList = this.cachedList.filter(c => c.typo === type);
 
     }
   }
 
 
-fromCreatedToDT(command){
-  console.log('COMANNNANZAFJAEJOF ', this.currentFile, this.commandPrice)
-  if(this.currentFile  !== '' && this.currentFile && this.commandPrice > 0) {
+fromCreatedToDT(command) {
+  console.log('COMANNNANZAFJAEJOF ', this.currentFile, this.commandPrice);
+  if (this.currentFile  !== '' && this.currentFile && this.commandPrice > 0) {
     const price = this.commandPrice.toString();
-    let params = new HttpParams().set("userId",command['userId']).set("commandId", command['id']).set("price", price); //Create new HttpParams
-    let formData: FormData = new FormData(); 
+    const params = new HttpParams().set('userId', command['userId']).set('commandId', command['id']).set('price', price); // Create new HttpParams
+    const formData: FormData = new FormData();
     formData.append('file', this.currentFile);
     this.loading = true;
     this.http.post('https://neon-server.herokuapp.com/fileUpload', formData, {params: params}).subscribe((user) => {
@@ -77,7 +70,7 @@ fromCreatedToDT(command){
       setTimeout(() => {
         this.fetchCommands();
         this.loading = false;
-        this.neonSelected = "none"
+        this.neonSelected = 'none';
       }, 1000);
     //       this.http.get('https://neon-server.herokuapp.com/users/' + command['userId']).subscribe((user) => {
     //   console.log('user:' , user)
@@ -99,25 +92,25 @@ fromCreatedToDT(command){
     //     console.log('cant find this command in user: ', updatedUser)
     //   }
     // })
- 
-    },err => console.log('err' , err))
+
+    }, err => console.log('err' , err));
   } else {
-    alert('Pour importer le DT il faut importer le fichier et entrer le prix du néon')
+    alert('Pour importer le DT il faut importer le fichier et entrer le prix du néon');
   }
 }
-deleteCommand(command){
-  const del = confirm("Confirmer vouloir effacer " + command.text);
-  if(del) {
-    if(command['userFull']) {
+deleteCommand(command) {
+  const del = confirm('Confirmer vouloir effacer ' + command.text);
+  if (del) {
+    if (command['userFull']) {
       command['userFull']['commands'] =  command['userFull']['commands'].filter(c => c.id !== command.id);
-      
+
       this.http.put('https://neon-server.herokuapp.com/users/' + command['userId'],  command['userFull']).subscribe((res) => {
         // this.fetchCommands();
         alert('Commande effacée');
         setTimeout(() => {
           this.fetchCommands();
         }, 1000);
-  
+
       }, err => {
         if (err.status === 200) {
           alert('Commande effacée');
@@ -128,65 +121,65 @@ deleteCommand(command){
         }
       });
     } else {
-      alert("cant delete")
+      alert('cant delete');
     }
   }
 
 }
 
 changePrice(price) {
-  if( price > 0) {
+  if ( price > 0) {
     this.commandPrice = price;
   } else {
-    alert('entre un prix valide petit coquin')
+    alert('entre un prix valide petit coquin');
   }
 }
-  fetchCommands(){
+  fetchCommands() {
     this.loading = true;
-    this.neonList = []
+    this.neonList = [];
     this.currentFile = null,
-    this.commandPrice = 0;  
-    this.numOfCommands = 0;  
-    this.numOfCreated = 0;  
-    this.numOfDT = 0;  
-    this.numOfPayes = 0;  
+    this.commandPrice = 0;
+    this.numOfCommands = 0;
+    this.numOfCreated = 0;
+    this.numOfDT = 0;
+    this.numOfPayes = 0;
 
     this.getConfig().subscribe((res: Array<Object>) => {
       this.loading = false;
-      if(res) {
+      if (res) {
         res.forEach((user) => {
-          if(user['commands'].length > 0) {
+          if (user['commands'].length > 0) {
            user['commands'].forEach((command) => {
              this.neonList.push({email: user['email'], type: user['type'], text: command ['text'], state: command['state'], price: command['price'], userId: user['id'], id: command['id'],
              commandInfo: command['commandInfo'], filePath: command['filePath'], userFull: user , typo: command['typo'],
               colors: command['colors'], height: command['height'], support: command['support']});
               this.numOfCommands++;
-              if(command['state'] === 'created') {
+              if (command['state'] === 'created') {
                   this.numOfCreated++;
               }
-              if(command['state'] === 'DT disponible') {
+              if (command['state'] === 'DT disponible') {
                 this.numOfDT++;
             }
-            if(command['state'] === 'payé') {
+            if (command['state'] === 'payé') {
               this.numOfPayes++;
           }
-           })
+           });
           }
-        })
+        });
       }
       console.log('User data after fetch : ', this.neonList);
-      this.cachedList = this.neonList
-    })
+      this.cachedList = this.neonList;
+    });
   }
 
   importDT(event, command) {
-    console.log('import DT. file :', event, ' command: ', command)
+    console.log('import DT. file :', event, ' command: ', command);
     this.currentFile = event.target.files[0];
-    
+
   }
 
   getConfig() {
-    return this.http.get('https://neon-server.herokuapp.com/users'); 
+    return this.http.get('https://neon-server.herokuapp.com/users');
   }
 
 
